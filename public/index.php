fcdbd38753788c95042a3aef0ddef47bf6f57612
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-06-07 10:54:31 +0200

    test

--- public/index.php
+++ public/index.php
@@ -136,7 +136,7 @@ $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
 	{
 	
 		$datajson=file_get_contents("chatbotdwt.json");
-		$myanswer=json_decode($datajson);
+		$myanswer=json_decode($dataschedule);
 		$message = $myanswer->{'type'};
 		$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
 		$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
