b1f8a48dfb88a2b98face6217823cfa7c2b0923d
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-06-07 16:00:19 +0200

    Retreived data from external json file

--- public/index.php
+++ public/index.php
@@ -135,7 +135,7 @@ $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
 	if(strtolower($userMessage) == 'schedule')
 	{
 		$datajson = file_get_contents('chatbotdwt.json');
-		$message = json_encode($datajson);
+		$message = json_decode($datajson);
 		$CarouselTemplateBuilder = new \LINE\LINEBot\MessageBuilder\CarouselTemplateBuilder($message);
 		$result = $bot->replyMessage($event['replyToken'], $CarouselTemplateBuilder);
 		return $result->getHTTPStatus() . ' ' . $result->getRawBody();
