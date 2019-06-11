5a1ba0952dbaed2a34417e448630daac5b53727c
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-06-11 16:37:47 +0200

    test

--- public/index.php
+++ public/index.php
@@ -157,77 +157,12 @@ $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
 	}	
 
 	if(strtolower($userMessage) == 'template')
+
 	{
-		$data={
-			"type": "template",
-			"altText": "this is a carousel template",
-			"template": {
-				"type": "carousel",
-				"columns": [
-					{
-					  "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-					  "imageBackgroundColor": "#FFFFFF",
-					  "title": "this is menu",
-					  "text": "description",
-					  "defaultAction": {
-						  "type": "uri",
-						  "label": "View detail",
-						  "uri": "http://example.com/page/123"
-					  },
-					  "actions": [
-						  {
-							  "type": "postback",
-							  "label": "Buy",
-							  "data": "action=buy&itemid=111"
-						  },
-						  {
-							  "type": "postback",
-							  "label": "Add to cart",
-							  "data": "action=add&itemid=111"
-						  },
-						  {
-							  "type": "uri",
-							  "label": "View detail",
-							  "uri": "http://example.com/page/111"
-						  }
-					  ]
-					},
-					{
-					  "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
-					  "imageBackgroundColor": "#000000",
-					  "title": "this is menu",
-					  "text": "description",
-					  "defaultAction": {
-						  "type": "uri",
-						  "label": "View detail",
-						  "uri": "http://example.com/page/222"
-					  },
-					  "actions": [
-						  {
-							  "type": "postback",
-							  "label": "Buy",
-							  "data": "action=buy&itemid=222"
-						  },
-						  {
-							  "type": "postback",
-							  "label": "Add to cart",
-							  "data": "action=add&itemid=222"
-						  },
-						  {
-							  "type": "uri",
-							  "label": "View detail",
-							  "uri": "http://example.com/page/222"
-						  }
-					  ]
-					}
-				],
-				"imageAspectRatio": "rectangle",
-				"imageSize": "cover"
-			}
-		  };
+		$data=file_get_contents('scndchatbotdwt.json');
 
 		$message=$data;
-		$mytemplate = new LINE\LINEBot\MessageBuilder\FlexMesssageBuilder($message);
+		$mytemplate = new LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder($message);
 		$result = $bot->replyMessage($event['replyToken'], $mytemplate);
 		return $result->getHTTPStatus() . ' ' . $result->getRawBody();
 	}	
