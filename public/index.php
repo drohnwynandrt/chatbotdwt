e6b562deeb5cb160f25d25ee69d678c3d577f5f7
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-06-11 09:47:42 +0200

    test

--- public/index.php
+++ public/index.php
@@ -132,41 +132,6 @@ $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
 	
 	}
 
-	if(strtolower($userMessage) == 'schedule')
-	{
-		$datajsonn={
-			"type": "template",
-			"altText": "this is a carousel template",
-			"template": {
-			  "type": "carousel",
-			  "actions": [],
-			  "columns": [
-				{
-				  "thumbnailImageUrl": "https://www.bostonmagazine.com/wp-content/uploads/sites/2/2018/10/massage-boston-1.jpg",
-				  "title": "Massage",
-				  "text": "Do you want to schedule a massage?",
-				  "actions": [
-					{
-					  "type": "datetimepicker",
-					  "label": "Schedule",
-					  "data": "Date",
-					  "mode": "datetime",
-					  "initial": "2019-06-06T15:32",
-					  "max": "2019-12-31T15:32",
-					  "min": "2018-06-06T15:32"
-					}
-				  ]
-				}
-			  ]
-			}
-		  };
-		$myanswer= json_decode($datajsonn);
-		$message = $datajsonn;
-		$newCTB = new LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($message);
-		$result = $bot->replyMessage($event['replyToken'], $newCTB);
-		return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-	
-	}
 	if(strtolower($userMessage) == 'ext')
 
 	{	$externalreply=file_get_contents('chatbotdwt.json');
