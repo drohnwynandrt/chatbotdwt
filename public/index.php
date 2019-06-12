e35c12326ed107e976c63c84db112e575b714a65
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-06-12 16:19:32 +0200

    test

--- public/index.php
+++ public/index.php
@@ -316,7 +316,8 @@ $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
 
 	{
         class otherrest extends FlexSampleRestaurant{
-            //otherrest->setURL = ('https://media-cdn.tripadvisor.com/media/photo-s/0e/cc/0a/dc/restaurant-chocolat.jpg');
+            return ImageComponentBuilder::builder()
+            ->setUrl('https://media-cdn.tripadvisor.com/media/photo-s/0e/cc/0a/dc/restaurant-chocolat.jpg')
         }
 		$data = otherrest::get();
 		file_put_contents('php://stderr', 'reply data: ' . print_r($data, true));
