0d56a4488d9258accd42844c62629a0a55e734fe
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-06-18 21:44:50 +0200

    test

--- public/index.php
+++ public/index.php
@@ -284,7 +284,7 @@ $signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
 		
         }
 
-        else (stripos($userMessage, "tarief") !== false)
+        else if (stripos($userMessage, "tarief") !== false)
 
 		{
 			$message = "Haar: 20 Euro\nFacemask: 30 Euro\nMassage: 15 Euro";
